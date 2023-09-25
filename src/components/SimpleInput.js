e8ecfede683ab269ce411a7c7e9691dccf409976
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2023-09-24 22:54:23 -0600

    Update SimpleInput.js
    - Test different ways to form submit and get user input values

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,6 +1,7 @@
 import { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
 
   const nameInputChangeHandler = (e) => {
@@ -9,13 +10,27 @@ const SimpleInput = (props) => {
 
   const formSubmissionHandler = (e) => {
     e.preventDefault();
+
+    console.log(enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    // nameInputRef.current.value = ""; //// this option is not ideal cause we are changing the DOM directly
+    setEnteredName("");
   };
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" onChange={nameInputChangeHandler} />
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
